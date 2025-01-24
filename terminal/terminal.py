09f46283c2e4db51b2f7d4ab5d69eff23766804c
DVorobyov <dvorobjevredstar@mail.ru>
2025-01-24 18:40:45 +0500

    first update plot

--- terminal/terminal.py
+++ terminal/terminal.py
@@ -87,20 +87,20 @@ class Terminal:
 
         five_min_ticks = ticks[(ticks['DATE'] >= last_write_date.strftime('%Y%m%d %H:%M:%S')) & (ticks['DATE'] < future_date.strftime('%Y%m%d %H:%M:%S'))]
 
-        new_candles = pd.DataFrame(columns=data.columns)
-        new_candles.loc[0, "TICKER"] = "SBER"
-        new_candles.loc[0, "DATE"] = future_date.strftime('%Y%m%d %H:%M:%S')
-        new_candles.loc[0, "OPEN"] = five_min_ticks.iloc[0]["OPEN"]
-        new_candles.loc[0, "HIGH"] = five_min_ticks["HIGH"].max()
-        new_candles.loc[0, "LOW"] = five_min_ticks["LOW"].min()
-        new_candles.loc[0, "CLOSE"] = five_min_ticks.iloc[-1]["CLOSE"]
-        new_candles.loc[0, "VOLUME"] = five_min_ticks["VOLUME"].sum()
-        print(new_candles)
+        new_candle = pd.DataFrame({
+            "TICKER": ["SBER"],
+            "DATE": [future_date.strftime('%Y%m%d %H:%M:%S')],
+            "OPEN": [five_min_ticks.iloc[0]["OPEN"]],
+            "HIGH": [five_min_ticks["HIGH"].max()],
+            "LOW": [five_min_ticks["LOW"].min()],
+            "CLOSE": [five_min_ticks.iloc[-1]["CLOSE"]],
+            "VOLUME": [five_min_ticks["VOLUME"].sum()],
+        }, columns=data.columns)
+        print(new_candle)
+        terminal_data = pd.concat([data, new_candle], ignore_index=True)
 
         with open('alor/tickers/SBER/config.json') as json_file:
             config = json.load(json_file)
-
-            terminal_data = pd.concat([data, new_candles], ).reset_index(drop=True)
 
             is_there_new_data = ticks['DATE'].max() > future_date
 
@@ -109,7 +109,7 @@ class Terminal:
                     indicator_data = pd.read_csv(f'terminal/data/SBER/indicators/{indicator["id"]}.csv')
                     start_index = len(indicator_data)-1
                     indicator_data = pd.concat([indicator_data.iloc[:-1], terminal_data.iloc[start_index:]])
-
+                    indicator_data = indicator_data.astype({col: 'float64' for col in indicator_data.columns if col not in ['TICKER', 'DATE']})
                     updated_indicator_data = super_trend.calculate_indicator(indicator_data, indicator, start_index)
                     updated_indicator_data = pd.concat([indicator_data.iloc[:start_index], updated_indicator_data.iloc[start_index:]])
                     updated_indicator_data = updated_indicator_data[updated_indicator_data.columns.drop('VOLUME')]
@@ -123,12 +123,12 @@ class Terminal:
                     # terminal_data.to_csv('terminal/data/SBER/sber.csv' , index=False)  # Save data to file
                     print('Done')
 
-            # terminal_data.set_index('date', inplace=True)
-            # terminal_data.index = pd.to_datetime(data.index).tz_localize('Etc/GMT-5')
-            # terminal_data = data.tail(100)
+            terminal_data.set_index('DATE', inplace=True)
+            terminal_data.index = pd.to_datetime(terminal_data.index).tz_localize('Etc/GMT-5')
+            terminal_data = terminal_data .tail(100)
 
-            # fplt.candlestick_ochl(data[['open', 'close', 'high', 'low']].tail(100))
-            # fplt.refresh()
+            fplt.candlestick_ochl(terminal_data[['OPEN', 'CLOSE', 'HIGH', 'LOW']])
+            fplt.refresh()
 
     def show(self):
         data = pd.read_csv('terminal/data/SBER/sber.csv')
